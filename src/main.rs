b99ea1b3599da9a57b7ec934e179fb2068d9fafd
Daniel Segal <dlemel8@gmail.com>
2020-12-24 08:41:23 +0200

    implement repeat parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use parser::Parser;
 use tls::tls_record_parser;
 
@@ -5,16 +7,12 @@ mod parser;
 mod tls;
 mod network;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let payload = "1603030025100000212043dcf44cc39c33ce2cda1a1a5106f249506d7519cdd1cb88b15f3594ca78277a14030300010116030300208e6041083b89ff49d068ec2f735d9dd8da0e286b5f9b84b135be0e4cf538c409";
     let payload = hex::decode(payload).expect("failed to decode payload");
-
-    let mut input = payload.as_slice();
-    let mut records = vec![];
-    let parser = tls_record_parser();
-    while let Ok(result) = parser.parse(input) {
-        records.push(result.parsed);
-        input = result.remaining;
-    }
-    println!("{:?}", records);
+    let parser = tls_record_parser().repeat(0..);
+    let records = parser.parse(payload.as_slice())?;
+    assert!(records.remaining.is_empty());
+    println!("{:?}", records.parsed);
+    Ok(())
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,5 @@
+use std::ops::{Bound, RangeBounds};
+
 #[derive(Debug)]
 pub struct ParserResult<'a, T> {
     pub parsed: T,
@@ -10,16 +12,20 @@ pub trait Parser<'a, T: 'a> {
     fn and<U: 'a>(self, another: impl Parser<'a, U> + 'a) -> BoxedParser<'a, (T, U)> where Self: Sized + 'a {
         BoxedParser { parser: Box::new(and(self, another)) }
     }
+
+    fn repeat(self, times: impl RangeBounds<usize> + 'a) -> BoxedParser<'a, Vec<T>> where Self: Sized + 'a {
+        BoxedParser { parser: Box::new(repeat(self, times)) }
+    }
 }
 
-// allow us to use closure as parser
+// allow us to use a closure as parser
 impl<'a, F, T: 'a> Parser<'a, T> for F where F: Fn(&'a [u8]) -> Result<ParserResult<'a, T>, String> {
     fn parse(&self, input: &'a [u8]) -> Result<ParserResult<'a, T>, String> {
         self(input)
     }
 }
 
-// allow us to use
+// allow us to use a combinator functions as part of Parser trait
 pub struct BoxedParser<'a, T> {
     parser: Box<dyn Parser<'a, T> + 'a>
 }
@@ -37,6 +43,31 @@ fn and<'a, A: 'a, B: 'a>(p1: impl Parser<'a, A>, p2: impl Parser<'a, B>) -> impl
                 ParserResult { parsed: (a, b), remaining: remaining2 }
             })
         })
+    }
+}
+
+fn repeat<'a, T: 'a>(p: impl Parser<'a, T>, times: impl RangeBounds<usize> + 'a) -> impl Parser<'a, Vec<T>> {
+    move |input: &'a [u8]| {
+        let limit = match times.end_bound() {
+            Bound::Excluded(&x) => x - 1,
+            Bound::Included(&x) => x,
+            _ => usize::MAX,
+        };
+
+        let mut records = vec![];
+        let mut internal_input = input;
+        while let Ok(result) = p.parse(internal_input) {
+            records.push(result.parsed);
+            internal_input = result.remaining;
+            if limit == records.len() {
+                break;
+            }
+        }
+
+        match times.start_bound() {
+            Bound::Included(&x) if x > records.len() => Err(format!("want at least {}, found {}", x, records.len())),
+            _ => Ok(ParserResult { parsed: records, remaining: internal_input })
+        }
     }
 }
 
@@ -103,6 +134,38 @@ mod tests {
                 .parse(b"abc")?;
         assert_eq!(((b'a', b'b'), b'c'), result.parsed);
         assert_eq!(b"", result.remaining);
+        Ok(())
+    }
+
+    #[test]
+    fn repeat_parser_failure() -> Result<(), Box<dyn Error>> {
+        let result = bytes_parser(b'a').repeat(1..).parse(b"ccc");
+        assert!(result.is_err());
+        Ok(())
+    }
+
+    #[test]
+    fn repeat_parser_success_empty() -> Result<(), Box<dyn Error>> {
+        let result = bytes_parser(b'a').repeat(..).parse(b"ccc")?;
+        let empty: Vec<u8> = vec![];
+        assert_eq!(empty, result.parsed);
+        assert_eq!(b"ccc", result.remaining);
+        Ok(())
+    }
+
+    #[test]
+    fn repeat_parser_success_unlimited() -> Result<(), Box<dyn Error>> {
+        let result = bytes_parser(b'a').repeat(1..).parse(b"aaaccc")?;
+        assert_eq!(vec![b'a', b'a', b'a'], result.parsed);
+        assert_eq!(b"ccc", result.remaining);
+        Ok(())
+    }
+
+    #[test]
+    fn repeat_parser_success_limited() -> Result<(), Box<dyn Error>> {
+        let result = bytes_parser(b'a').repeat(..=2).parse(b"aaaccc")?;
+        assert_eq!(vec![b'a', b'a'], result.parsed);
+        assert_eq!(b"accc", result.remaining);
         Ok(())
     }
 }
